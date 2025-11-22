e85636d1b90f9be18b7abee438bfd4c10741d054
Jatin-dev12 <jatinsehgal124@gmail.com>
2025-11-22 21:02:53 +0530

    Logo

--- components/Common/Intro/index.jsx
+++ components/Common/Intro/index.jsx
@@ -8,7 +8,7 @@ import Tools from './Tools';
 import Skills from './Skills';
 import Image from 'next/image';
 import { NAME, DESIGNATION, SOCIAL_LINKS } from '../../../constants/constants';
-import Jatin from '../../../public/images/Logo.jpg';
+import Jatin from '../../../public/images/jatin.jpg';
 
 const Intro = () => {
   return (
