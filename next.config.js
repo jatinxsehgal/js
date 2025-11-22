2489ef9406b4316a93715c38c4123fb950d78411
Jatin-dev12 <jatinsehgal124@gmail.com>
2025-11-22 22:12:52 +0530

    red

--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ module.exports = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    async redirects() {
+    return [
+      {
+        source: 'https://sehgal.netlify.app/',   // old URL
+        destination: 'https://sehgal.vercel.app/', // new URL
+        permanent: true,       // true = 308 redirect (SEO friendly)
+      },
+    ];
+  },
   },
 }
